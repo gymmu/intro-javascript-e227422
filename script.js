64d98c77b42640a69fe2ddf2c966816fd7a39809
Aylina Ospina Cruz <e227422@edu.sbl.ch>
2023-10-16 12:07:12 +0200

    Aufgabe 3

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function changeBackground() {
   const body = document.querySelector("body")
   body.style.backgroundColor = "darkgrey"
 }
+
+
+function deleteText() {
+  const elemToDeleteText = document.querySelector("#to-delete")
+  elemToDeleteText.textContent = "Inhalt gelöscht"
+}
+
+function plusThree() {
+  const numberElement = document.querySelector("#zahl")
+  let num = parseInt(numberElement.textContent)
+  num = num + 3
+  numberElement.textContent = num
+}
